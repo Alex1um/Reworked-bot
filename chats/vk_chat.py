7b7bc6fac441474d0787bd1cefc7fc1c476ad7c9
assa <ye4h@protonmail.com>
2020-05-05 16:32:42 +0700

    pep8 + correcting forwarding

--- chats/vk_chat.py
+++ chats/vk_chat.py
@@ -19,7 +19,8 @@ class VK(Chat):
 
     def input(self, res, id):
         try:
-            if res is None or len(res['updates']) == 0:  # checking for right response
+            if res is None or len(
+                    res['updates']) == 0:  # checking for right response
                 pass
             else:
                 for update in res['updates']:
@@ -42,7 +43,8 @@ class VK(Chat):
                                           message=text,
                                           v=self.api_version,
                                           random_id=rid,
-                                          attachment=attachment)  # sending message
+                                          attachment=attachment
+                                          )  # sending message
             elif isinstance(text, GeneratorType):
                 a = True
                 for msg in text:
@@ -51,10 +53,16 @@ class VK(Chat):
                                                           message=msg,
                                                           v=self.api_version,
                                                           random_id=rid,
-                                                          attachment=attachment)  # sending message
+                                                          attachment=attachment
+                                                          )  # sending message
                         a = False
                     else:
-                        self.vk_api.messages.edit(peer_id=id, message=msg, v=self.api_version, message_id=outid, attachment=attachment)
+                        self.vk_api.messages.edit(
+                            peer_id=id,
+                            message=msg,
+                            v=self.api_version,
+                            message_id=outid,
+                            attachment=attachment)
 
     def __init__(self, token, _group_id, v, main_system: ChatSystem):
         super().__init__(main_system)
@@ -64,7 +72,9 @@ class VK(Chat):
         self.get_server()
 
     def get_server(self):
-        self.LPS = self.vk_api.groups.getLongPollServer(group_id=self.group_id, v=self.api_version)  # getting server
+        self.LPS = self.vk_api.groups.getLongPollServer(
+            group_id=self.group_id,
+            v=self.api_version)  # getting server
 
     def send_requsest(self, ts):
         link = f'{self.LPS["server"]}?act=a_check&key={self.LPS["key"]}&ts={ts}&wait=25'  # setting link
@@ -89,11 +99,22 @@ class VK(Chat):
 
     def message_parse(self, res):
         r_msg = ''
-        attachments = res['object']['message']['attachments'] if res['object']['message']['attachments'] else []
-        if '-fwd' not in res['object']['message']['text'] or not res['object']['message']['fwd_messages']:  # for forward messages
+        r_userid = res['object']['message']['from_id']  # who send
+        session = self.main_system.db_session.create_session()
+        is_fwding = session.query(self.main_system.db_session.Settings).filter(
+            (self.main_system.db_session.Settings.user_id == r_userid) & (
+                    self.main_system.db_session.Settings.name == "enable_fwd")
+        ).first()
+        attachments = res['object']['message']['attachments'] if res[
+            'object']['message']['attachments'] else []
+        if is_fwding is None or\
+                '-fwd' not in res['object']['message']['text'] or \
+                not res['object']['message'][
+                    'fwd_messages']:  # for forward messages
             r_msg = res['object']['message']['text'].strip()
         else:  # для вложенных сообщений
-            r_msg = res['object']['message']['text'][:res['object']['message']['text'].find('-fwd')]
+            r_msg = res['object']['message']['text'][
+                    :res['object']['message']['text'].find('-fwd')]
 
             def find_fwd(fwd):
                 msg = ''
@@ -113,28 +134,36 @@ class VK(Chat):
             r_msg += ans[0]
             attachments.extend(ans[1])
 
-            r_msg += res['object']['message']['text'][res['object']['message']['text'].find('-fwd') + 4::]
+            r_msg += res['object']['message']['text'][
+                     res['object']['message']['text'].find('-fwd') + 4::]
         r_date = res['object']['message']['date']  # date
         r_sendid = res['object']['message']['peer_id']  # from send
-        r_userid = res['object']['message']['from_id']  # who send
         r_ctype = 'vk'
         r_attachments = {'image': [], 'sound': [], 'doc': []}
         for attachment in attachments:  # attachments
             if attachment['type'] == 'photo':
-                r_attachments['image'].append(attachment['photo']['sizes'][-1]['url'])
+                r_attachments['image'].append(
+                    attachment['photo']['sizes'][-1]['url'])
             elif attachment['type'] == 'doc':
                 if attachment['doc']['ext'] in {'wav', 'mp3'}:
-                    r_attachments['sound'].append((attachment['doc']['url'], attachment['doc']['ext']))  # 0: link; 1: extension
+                    r_attachments['sound'].append(
+                        (attachment['doc']['url'],
+                         attachment['doc']['ext']))  # 0: link; 1: extension
             elif attachment['type'] == 'audio':
-                r_attachments['sound'].append((attachment['audio']['url'], 'mp3'))
+                r_attachments['sound'].append(
+                    (attachment['audio']['url'], 'mp3'))
             elif attachment['type'] == 'audio_message':
-                r_attachments['sound'].append((attachment['audio_message']['link_mp3'], 'mp3'))
+                r_attachments['sound'].append(
+                    (attachment['audio_message']['link_mp3'], 'mp3'))
         dbs = self.main_system.db_session
         session = dbs.create_session()
-        set = session.query(dbs.Settings).filter((dbs.Settings.user_id == r_sendid) & (dbs.Settings.name == "active")).first()
+        set = session.query(dbs.Settings).filter(
+            (dbs.Settings.user_id == r_sendid) & (
+                    dbs.Settings.name == "active")).first()
         if set:
             if r_msg.find('end') != -1:  # exit from active commands
-                if r_msg[:r_msg.find('end')] in self.main_system.defaut_command_symbols:
+                if r_msg[:r_msg.find(
+                        'end')] in self.main_system.defaut_command_symbols:
                     set.delete()
                     session.commit()
             else:
