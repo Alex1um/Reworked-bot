206af7f9b01d6a4b71c8a1add0b60108d516b7cb
assa <ye4h@protonmail.com>
2020-05-07 22:11:53 +0700

    fixes settings of salad files fixed description of stupid ai

--- commands/salades.py
+++ commands/salades.py
@@ -35,13 +35,13 @@ def salades_op(params, system: ChatSystem, message):
         return file if file else 'food'
     elif prev_param == 'switch' and params:
         if params[0] in map(lambda x: x[x.rfind('/') + 1:x.rfind('.'):],
-                            fix_path(glob.glob("commands/files/*.saladict"))):
+                            fix_paths(glob.glob("commands/files/*.saladict"))):
             file = params[0]
         else:
             return 'Файл не найден'
     elif prev_param == 'list':
         return '\n'.join(map(lambda x: x[x.rfind('/') + 1:x.rfind('.'):],
-                             fix_path(glob.glob("commands/files/*.saladict"))))
+                             fix_paths(glob.glob("commands/files/*.saladict"))))
     elif prev_param == 'default':
         session.delete(salades_file)
         file = None
